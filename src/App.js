ec80c934a2820af7652d4622aee6b0d6d1314a07
serhatbek <serhat.bekk@gmail.com>
2022-08-17 12:04:22 +0300

    add data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 
+import { useState } from 'react';
+import menu from './data';
+
 function App() {
-  return <div className='App'>Menu</div>;
+  const [menuItems, setMenuItems] = useState(menu);
+  console.log(menuItems);
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
