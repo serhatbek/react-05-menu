5db3cac36dbff4a340b9f04b6a68dc3d7b405849
serhatbek <serhat.bekk@gmail.com>
2022-08-17 17:48:40 +0300

    add filter buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 
 import { useState } from 'react';
-import menu from './data';
 import Categories from './components/Categories';
 import Menu from './components/Menu';
+import items from './data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
-  const [menuItems, setMenuItems] = useState(menu);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      return setMenuItems(items);
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -15,7 +27,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
